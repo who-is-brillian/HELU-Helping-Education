6ceebd17c58ecd0f65fc6ebe15438a4561088d7a
who-is-brillian <brillianmk86@gmail.com>
2024-10-13 16:53:49 +0700

    revisi

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-// Ini untuk Icon social Footer
-const ytIcons = document.getElementById('youtube');
-const fbIcons = document.getElementById('facebook');
-const liIcons = document.getElementById('linkedin');
-const waIcons = document.getElementById('whatsapp');
-const EmIcons = document.getElementById('email');
-const textYt = document.getElementById('textYt');
-const textFb = document.getElementById('textFb');
-const textLi = document.getElementById('textLi');
-const textWa = document.getElementById('textWa');
-const textEm = document.getElementById('textEm');
-function social(icons, textIcons){
-icons.addEventListener('click', function(){
-   textIcons.classList.remove('d-none')
-})
-}
 
-social(ytIcons, textYt)
-social(fbIcons, textFb)
-social(liIcons, textLi)
-social(waIcons, textWa)
-social(EmIcons, textEm)
-
-const flipCard1 = document.getElementById('card-1');
-const flipCard2 = document.getElementById('card-2');
-const flipCard3 = document.getElementById('card-3');
-const flipCard4 = document.getElementById('card-4');
-const nameMentors1 = document.querySelectorAll(".nameMentor-1");
-const nameMentors2 = document.querySelectorAll(".nameMentor-2");
-const nameMentors3 = document.querySelectorAll(".nameMentor-3");
-const nameMentors4 = document.querySelectorAll(".nameMentor-4");
 
 // Fungsi untuk Card Mentor
 function toggleVisibility(elements) {
